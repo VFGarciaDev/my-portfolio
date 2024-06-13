99777dd64d1dea2f60b58f55550ad169f83cd4fa
VGarcia <vfeitosa.garcia@gmail.com>
2024-06-13 00:31:41 -0300

    feat: add Header and Home components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { ThemeProvider } from './components/theme-provider/theme-provider'
 import './styles/App.css'
+import { ThemeProvider } from './components/theme-provider/theme-provider'
+import { Header } from './components/header/header'
+import { Footer } from './components/footer/footer'
+import { Home } from './components/hero/home'
+// import { About } from './components/about/about'
 
 function App() {
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme" children={undefined}>
-      
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Header />
+        <Home />
+        {/* <About /> */}
+
+        <Footer />
     </ThemeProvider>
   )
 }
