6caf4601c983b569d6260ca02b3c896222464962
VGarcia <vfeitosa.garcia@gmail.com>
2024-06-14 11:55:26 -0300

    feat: add components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './styles/App.css'
 import { ThemeProvider } from './components/theme-provider/theme-provider'
 import { Header } from './components/header/header'
-import { Footer } from './components/footer/footer'
 import { Home } from './components/hero/home'
-// import { About } from './components/about/about'
+import { About } from './components/about/about'
+import { Contact } from './components/contact/contact'
+import { Footer } from './components/footer/footer'
 
 function App() {
 
@@ -11,8 +12,9 @@ function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Header />
         <Home />
-        {/* <About /> */}
+        <About />
 
+        <Contact />
         <Footer />
     </ThemeProvider>
   )
